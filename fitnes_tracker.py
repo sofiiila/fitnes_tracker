313cc99255ffbb36ebde0af984c52910e565b8bb
Глеб Лазарев <sofi@sofi.com>
2023-12-26 20:00:30 -0800

    заработало

--- fitnes_tracker.py
+++ fitnes_tracker.py
@@ -12,17 +12,18 @@ class InfoMessage:
     calories: float
 
     def get_message(self) -> str:
-        return(f'Тип тренировки: {self.training_type};
-               fДлительность: {self.duration:.3f} ч;
-               Дистанция: {self.distance:.3f} км;
-               Ср. скорость: {self.speed:.3f} км/ч;
-               Потрачено ккал: {self.calories:.3f}.')
+        return (           
+            f'Тип тренировки: {self.training_type}; ' 
+            f'Длительность: {self.duration:.3f} ч.; ' 
+            f'Дистанция: {self.distance:.3f} км; ' 
+            f'Ср. скорость: {self.speed:.3f} км/ч; ' 
+            f'Потрачено ккал: {self.calories:.3f}.') 
 
 @dataclass     
 class Training:
     """Базовый класс тренировки"""
     M_IN_KM = 1000
-    LEN_STEP = 0,65   
+    LEN_STEP = 0.65   
 
     action: int
     duration: float
@@ -36,7 +37,7 @@ class Training:
     @property    
     def get_mean_speed(self) -> float:
         """Вычисление средней скорости."""
-        return self.get_distance() / self.duration
+        return self.get_distance / self.duration
         
     def get_spent_calories(self) -> float:
         """ВЫчисление затраченных калорий"""
@@ -47,16 +48,16 @@ class Training:
         return InfoMessage( 
             type(self).__name__,  
             self.duration, 
-            self.get_distance(), 
-            self.get_mean_speed(), 
-            self.get_spent_calories())  
+            self.get_distance, 
+            self.get_mean_speed, 
+            self.get_spent_calories)  
       
 class Running(Training):
     """Вид тренировки: бег."""
   
     @property
     def get_spent_calories(self) -> float:
-          return ((18 * self.get_mean_speed() + 1.79) * self.weight / self.M_IN_KM * self.duration)
+          return ((18 * self.get_mean_speed + 1.79) * self.weight / self.M_IN_KM * self.duration)
     
 class SportsWalking(Training):  
     """Вид тренировки: спортивная ходьба."""  
@@ -66,11 +67,11 @@ class SportsWalking(Training):
 
     @property
     def get_spent_calories(self):
-          return ((0.035 * self.weight + (self.get_mean_speed() ** 2 / self.height) * 0.029 * self.weight) * self.duration)
+          return ((0.035 * self.weight + (self.get_mean_speed ** 2 / self.height) * 0.029 * self.weight) * self.duration)
 
 class Swimming(Training):
     """Вид тренировки: плавание."""
-    LEN_STROKE = 1.38
+    LEN_STEP = 1.38
     
     def __init__(self, action: int, duration: float, weight: float, length_pool: float, count_pool: float):
           super().__init__(action, duration, weight)
@@ -83,59 +84,43 @@ class Swimming(Training):
 
     @property 
     def get_spent_calories(self):
-          return (self.get_mean_speed() + 1.1) * 2 * self.weight
+          return (self.get_mean_speed + 1.1) * 2 * self.weight
 
-    def read_package(workout_type: str, data: list) -> Training:
-        """Получение данных от датчиков."""
-        try:
-             if workout_type == "SWM":
-                  return Swimming(
-                       data[0],
-                       data[1],
-                       data[2],
-                       data[3],
-                       data[4])
-             elif workout_type == "RUN":
-                  return Running(
-                       data[0],
-                       data[1],
-                       data[2])
-             elif workout_type == "WLK":
-                  return SportsWalking(
-                       data[0],
-                       data[1],
-                       data[2],
-                       data[3])
-        except IndexError:
-             print("Ошибка чтения с датчиков")
+def read_package(workout_type: str, data: list) -> Training: 
+    """Прочитать данные полученные от датчиков.""" 
+    parameters_train = { 
+        'SWM': Swimming, 
+        'RUN': Running, 
+        'WLK': SportsWalking} 
+    if workout_type in parameters_train: 
+        return parameters_train[workout_type](*data) 
+    else: 
+        pass 
 
-    def main(Training) -> None:
-        """Главная функция"""
-        try:
-            message_train = Training.show_training_info()
-            print(message_train.get_message())
-        except AttributeError:
-            print("Данный тип тренировки неизвестен")
-        pass
+def main(training: Training) -> None:
+    """Главная функция"""
+    try:
+        message_train = training.show_training_info()
+        print(message_train.get_message())
+    except AttributeError:
+        print("Данный тип тренировки неизвестен")
+    pass
 
-    if __name__ == "__main__":
-         packages = [
-              ("SWM", [720, 1, 80, 25, 40]),
-              ("RUN", [15000, 1, 75]),
-              ("WLK", [9000, 1, 75, 180]),
-         ]
+if __name__ == "__main__":
+    packages = [        
+        ("SWM", [720, 1, 80, 25, 40]),
+        ("RUN", [15000, 1, 75]),
+        ("WLK", [9000, 1, 75, 180]),
+    ]
 
-    for i in range(len(packages)):
-        if len(packages[i]) == 2:
-            workout_type = packages[i][0] 
-            data = packages[i][1] 
-            if packages[i][1][0] > 0: 
-                training = read_package(workout_type, data) 
-                main(training) 
-            else: 
-                print('Вы еще не начали тренироваться=)') 
-        else: 
-            print('Не определен тип тренировки или датчики неисправны') 
+    for workout_type, data in packages:       
+        if data[0] > 0:
+            training = read_package(workout_type, data)
+            main(training)
+        else:
+            print('Вы еще не начали тренироваться=)')
+    else:
+        print('Не определен тип тренировки или датчики неисправны')
                 
 
         
